ea9924e8f054267e22655c01f7d957f1a21f3714
Will Smith <william.smith29@outlook.com>
2026-08-27 11:37:41 +0100

    auto-populate holiday slug

--- holidays/models.py
+++ holidays/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.text import slugify
 
 class Holiday(models.Model):
     name = models.CharField(max_length=100, unique=True)
@@ -10,6 +11,11 @@ class Holiday(models.Model):
     cover_image = models.ImageField(upload_to='holiday_covers/', blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     last_modified_at = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return f"{self.name} ({self.start_date} to {self.end_date})"
