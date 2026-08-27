5e5d845fb3f36374a21db640ccf1aecc7911dce5
Will Smith <william.smith29@outlook.com>
2026-08-27 10:45:17 +0100

    holiday model init

--- holidays/models.py
+++ holidays/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Holiday(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(unique=True)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    description = models.TextField(blank=True, null=True)
+    status = models.CharField(max_length=20, choices=[('planned', 'Planned'), ('upcoming', 'Upcoming'), ('completed', 'Completed')], default='planned')
+    cover_image = models.ImageField(upload_to='holiday_covers/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_modified_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.name} ({self.start_date} to {self.end_date})"
