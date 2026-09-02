4df0abb1d1307a492fe59fd13773044cd8f65889
Will Smith <william.smith29@outlook.com>
2026-09-02 16:23:20 +0100

    profile model and view

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import UserCreateView
+from .views import UserCreateView, ProfileDetailView
 
 # Ensure there's no clashes with django.contrib.auth.urls
 urlpatterns = [
     path('sign-up/', UserCreateView.as_view(), name='user-create'),
+    path('profile/', ProfileDetailView.as_view(), name='profile-detail'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
 class UserCreateView(CreateView):
     form_class = UserCreationForm
     template_name = "users/user_create.html"
     success_url = reverse_lazy('login')
+
+class ProfileDetailView(LoginRequiredMixin, DetailView):
+    model = Profile
+
+    def get_object(self):
+        return self.request.user.profile
