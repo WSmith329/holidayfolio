37a9be2976a6b2e8f1bc3388aa5426e9033af8ba
Will Smith <william.smith29@outlook.com>
2026-09-02 14:42:33 +0100

    django built-in authentication views

--- config/settings.py
+++ config/settings.py
@@ -126,3 +126,12 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ # Email backend configuration for development
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+# Login and logout redirect URLs
+
+LOGIN_REDIRECT_URL = "holiday-list"
+LOGOUT_REDIRECT_URL = "login"
