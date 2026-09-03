90e43874e2927c75707ace19dd2444e0ef00fb1d
Will Smith <william.smith29@outlook.com>
2026-09-03 09:23:04 +0100

    holidays views login required

--- holidays/views.py
+++ holidays/views.py
@@ -1,20 +1,21 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Holiday
 
 # Display views
-class HolidayListView(ListView):
+class HolidayListView(LoginRequiredMixin, ListView):
     model = Holiday
 
     def get_queryset(self):
         return Holiday.objects.filter(owner=self.request.user)
 
-class HolidayDetailView(DetailView):
+class HolidayDetailView(LoginRequiredMixin, DetailView):
     model = Holiday
 
 # Editing views
-class HolidayCreateView(CreateView):
+class HolidayCreateView(LoginRequiredMixin, CreateView):
     model = Holiday
     fields = ['name', 'start_date', 'end_date', 'description', 'cover_image']
 
@@ -22,10 +23,10 @@ class HolidayCreateView(CreateView):
         form.instance.owner = self.request.user
         return super().form_valid(form)
 
-class HolidayUpdateView(UpdateView):
+class HolidayUpdateView(LoginRequiredMixin, UpdateView):
     model = Holiday
     fields = ['name', 'start_date', 'end_date', 'description', 'cover_image']
 
-class HolidayDeleteView(DeleteView):
+class HolidayDeleteView(LoginRequiredMixin, DeleteView):
     model = Holiday
     success_url = reverse_lazy('holiday-list')
